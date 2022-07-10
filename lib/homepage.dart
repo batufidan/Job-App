25f81270dadb3c94555a03ef2456d4990a5b5664
Batuhan <batuhan.fidan@ogr.iu.edu.tr>
2022-07-10 21:31:10 +0300

    first commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:job_app/util/recent_job_card.dart';
+
+import 'util/job_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +11,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List jobsForYou = [
+    ['Uber', 'UI Designer', 'lib/icons/uber.png', 45],
+    ['Google', 'Product Dev', 'lib/icons/google.png', 80],
+    ['Uber', 'Software Eng.', 'lib/icons/apple.png', 95],
+  ];
+
+  final List recentJobs = [
+    ['Nike', 'Web Designer', 'lib/icons/nike.png', 45],
+    ['Apple', 'Software Eng.', 'lib/icons/apple.png', 95],
+    ['Google', 'Product Dev.', 'lib/icons/google.png', 44],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +31,7 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            height: 75,
+            height: 45,
           ),
           //app bar
           Padding(
@@ -61,42 +76,130 @@ class _HomePageState extends State<HomePage> {
           // search bar
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(
-                  color: Colors.white,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                            height: 30,
+                            child: Image.asset(
+                              'lib/icons/search.png',
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search for a job..',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Container(
-                      height: 30,
-                      child: Image.asset(
-                        'lib/icons/search.png',
-                        color: Colors.grey[600],
-                      ),
-                    ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 50,
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[800],
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search for a job..',
-                      ),
-                    ),
+                  child: Image.asset(
+                    'lib/icons/preferences.jpg',
+                    color: Colors.white,
                   ),
-                ],
-              ),
+                )
+              ],
             ),
+          ),
+
+          SizedBox(
+            height: 30,
           ),
 
           // for you -> job cards
 
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Text(
+              'For You',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+          ),
+
+          SizedBox(
+            height: 25,
+          ),
+
+          Container(
+            height: 160,
+            child: ListView.builder(
+              itemCount: jobsForYou.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return JobCard(
+                  companyName: jobsForYou[index][0],
+                  jobTitle: jobsForYou[index][1],
+                  logoImagePath: jobsForYou[index][2],
+                  hourlyRate: jobsForYou[index][3],
+                );
+              },
+            ),
+          ),
+
+          SizedBox(
+            height: 30,
+          ),
+
           // recently add -> job titles
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Text(
+              'Recentyle Added',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ListView.builder(
+                itemCount: recentJobs.length,
+                itemBuilder: (context, index) {
+                  return RecentJobCard(
+                    companyName: recentJobs[index][0],
+                    jobTitle: recentJobs[index][1],
+                    logoImagePath: recentJobs[index][2],
+                    hourlyRate: recentJobs[index][3],
+                  );
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
